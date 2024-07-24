3bf4cdf266e682bcf34d8d7d7038bc5a60e7fec0
Buddhi19 <wijenayakebuddhi34802@gmail.com>
2024-07-24 17:27:30 +0530

    feat(ANALYSIS): Added analysis for other diseases - updated gitignore to only upload required pngs - added correlation details for each disease

---
+++ ANALYSIS_FOR_OTHER_DISEASES/main.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from ANALYSIS_FOR_OTHER_DISEASES.Analysis_of_Death_Data import MORTALITY_DATA
+
+
+def main():
+    year = input("Enter the year(YYYY) or enter time period(YYYY-YYYY): ")
+    if '-' in year:
+        year = year.split('-')
+        mortality_data = MORTALITY_DATA.ADD_RANGE(int(year[0]), int(year[1]))
+    else:
+        mortality_data = MORTALITY_DATA(int(year))
+    mortality_data.ANALYZER()

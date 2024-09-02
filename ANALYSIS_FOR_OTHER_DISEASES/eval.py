deebe860ebb5a8d5d6b250cbcd96fbfe4f3c2fd7
Buddhi19 <wijenayakebuddhi34802@gmail.com>
2024-09-02 19:51:53 +0530

    refactor: Update EVAL function to dynamically load correlation data
    The EVAL function in eval.py has been updated to dynamically load the correlation data based on the country name provided as an argument. This allows for more flexibility in analyzing correlation coefficients for different countries. The DATA_PATH variable has been modified to include the country name in the file path. This change improves the reusability and maintainability of the code.

--- ANALYSIS_FOR_OTHER_DISEASES/eval.py
+++ ANALYSIS_FOR_OTHER_DISEASES/eval.py
@@ -5,14 +5,13 @@ import pandas as pd
 main_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(main_dir)
 
-DATA_PATH = os.path.join(main_dir, 'RESULTS/CORRELATION_DATA_FOR_OTHER_DISEASES/Correlation_Coefficient_moldova.csv')
 
-def EVAL():
+def EVAL(name: str = 'japan'):
+    DATA_PATH = os.path.join(main_dir, f'RESULTS/CORRELATION_DATA_FOR_OTHER_DISEASES/Correlation_Coefficient_{name}.csv')
     data = pd.read_csv(DATA_PATH)
     count = 0
     for disease in data['Cause of Death'].unique():
         data_per_disease = data[data['Cause of Death'] == disease]
-        # if max of r squared value of parameters is popstat then print it
         if data_per_disease['r squared value'].max() == data_per_disease[data_per_disease['Parameter'] == 'POPSTAT_COVID19']['r squared value'].values[0]:
             print(f"{disease} is best fitted with POPSTAT with R squared value of {data_per_disease['r squared value'].max()}")
             count += 1
--- ANALYSIS_FOR_OTHER_DISEASES/main.py
+++ ANALYSIS_FOR_OTHER_DISEASES/main.py
@@ -5,6 +5,11 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from ANALYSIS_FOR_OTHER_DISEASES.Analysis_of_Death_Data import MORTALITY_DATA
 from ANALYSIS_FOR_OTHER_DISEASES.Death_data_Processor import DEATH_DATA_PROCESSOR
+from ANALYSIS_FOR_OTHER_DISEASES.eval import EVAL
+
+from ANALYSIS.Population_Data_For_Date import POPULATION_DATA_FOR_DATE
+from ANALYSIS.Covid_Data_For_Date import COVID_DATA_FOR_DATE
+from ANALYSIS.Pop_Stat_Calculation import POP_STAT_CALCULATION
 
 class ANALYSIS_FOR_OTHER_DISEASES:
     def __init__(self):
@@ -12,10 +17,15 @@ class ANALYSIS_FOR_OTHER_DISEASES:
 
     def run(self):
         death_year = int(input('Enter Considering Death data year: '))
+        COVID_DATA_FOR_DATE("2023-05-05")
+        POPULATION_DATA_FOR_DATE(death_year)
+        POP_CAL = POP_STAT_CALCULATION()
+        POP_CAL.run()
         reference_country = input('Enter reference country: ')
         DEATH_DATA_PROCESSOR(death_year)
         data = MORTALITY_DATA(death_year, reference_country)
         data.ANALYZER()
+        EVAL(reference_country)
 
     def parser_run(self, death_year, reference_country, selected: bool):
         DEATH_DATA_PROCESSOR(death_year)

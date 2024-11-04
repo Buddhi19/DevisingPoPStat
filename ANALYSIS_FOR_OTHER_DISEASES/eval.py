6de118a9fa62e439898ffab65d3048cbb2d3838f
Buddhi19 <wijenayakebuddhi34802@gmail.com>
2024-11-04 22:35:05 +0530

    Feat: Add function to convert death data into structured DataFrame and save results

--- ANALYSIS_FOR_OTHER_DISEASES/eval.py
+++ ANALYSIS_FOR_OTHER_DISEASES/eval.py
@@ -6,6 +6,7 @@ main_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(main_dir)
 
 DATA_PATH = os.path.join(main_dir,'RESULTS/CORRELATION_DATA_FOR_OTHER_DISEASES/Correlation_Coefficient_custom.csv')
+GLOBAL_DEATH_DATA_PATH = os.path.join(main_dir, 'DATA/death_data/Global_Deaths.csv')
 
 DATAFRAME = {
         'Cause of Death': [],
@@ -58,10 +59,67 @@ def EVAL_FOR_BEST(num: int):
         
     print(f"Total of {count} diseases are ranked {num} with POPSTAT")
 
+def convert_to_DATAFRAME(path:str):
+    data = pd.read_csv(path)
+    DATAFRAME_WITH_DEATH_COUNT = {
+        'Cause of Death': [],
+        'PoPStat': [],
+        'PoPStat R value' : [],
+        'Reference': [],
+        'Death Count': [],
+        'HDI': [],
+        'SDI': [],
+        'Median Age': [],
+        'GDP per capita': [],
+        'Population Density': [],
+        'Gini coefficient': [],
+        'UHCI': [],
+        'Life expectancy': []
+    }
+    global_death_data = pd.read_csv(GLOBAL_DEATH_DATA_PATH)
+    for disease in data['Cause of Death'].unique():
+        data_per_disease = data[data['Cause of Death'] == disease]
+        PoPStat_data = data_per_disease[data_per_disease['Parameter'] == f'POPSTAT_{disease}']['r squared value']
+        if PoPStat_data.empty:
+            continue
+        PoPStat_data = PoPStat_data.values[0]
+        DATAFRAME_WITH_DEATH_COUNT['Cause of Death'].append(disease)
+        global_death_per_disease = global_death_data[global_death_data['cause_name'] == disease]
+        if global_death_per_disease.empty:
+            DATAFRAME_WITH_DEATH_COUNT['Death Count'].append(0)
+        else:
+            global_death_count = global_death_per_disease[
+                global_death_per_disease['metric_name'] == 'Rate'
+            ]['val'].values[0]
+            DATAFRAME_WITH_DEATH_COUNT['Death Count'].append(global_death_count)
+        
+        PoPStat_R_val = data_per_disease[data_per_disease['Parameter'] == f'POPSTAT_{disease}']['R value'].values[0]
+        DATAFRAME_WITH_DEATH_COUNT['PoPStat'].append(PoPStat_data)
+        DATAFRAME_WITH_DEATH_COUNT['Reference'].append(
+            data_per_disease[data_per_disease['Parameter'] == f'POPSTAT_{disease}']['reference_country'].values[0]
+        )
+        DATAFRAME_WITH_DEATH_COUNT['PoPStat R value'].append(PoPStat_R_val)
+        for parameter in [
+            'HDI', 'SDI', 'Median Age', 'GDP per capita', 'Population Density', 'Gini coefficient', 'UHCI', 'Life expectancy'
+        ]:
+            try:
+                DATAFRAME_WITH_DEATH_COUNT[parameter].append(
+                    data_per_disease[data_per_disease['Parameter'] == parameter]['r squared value'].values[0]
+                )
+            except:
+                DATAFRAME_WITH_DEATH_COUNT[parameter].append(0)
+
+    DATAFRAME_WITH_DEATH_COUNT = pd.DataFrame(DATAFRAME_WITH_DEATH_COUNT)
+    DATAFRAME_WITH_DEATH_COUNT = DATAFRAME_WITH_DEATH_COUNT.sort_values(by='Death Count', ascending=False)
+    pd.DataFrame(DATAFRAME_WITH_DEATH_COUNT).to_csv(f'RESULTS/POPSTAT_OTHER_DISEASES/COUNTING/POPSTAT_ALL_with_death_count.csv', index=False)
+
+
+def convert_communicable():
+    # data_path = os.path.join(main_dir, 'RESULTS/CORRELATION_DATA_FOR_OTHER_DISEASES/Correlation_Coefficient_communicable.csv')
+    data_path = DATA_PATH
+    convert_to_DATAFRAME(data_path)
+    return
+
 
 if __name__ == '__main__':
-    INDEX = 6
-    EVAL_FOR_BEST(INDEX)
-    DATAFRAME = pd.DataFrame(DATAFRAME)
-    DATAFRAME = DATAFRAME.sort_values(by='PoPStat', ascending=False)
-    pd.DataFrame(DATAFRAME).to_csv(f'RESULTS/POPSTAT_OTHER_DISEASES/Best_Results_{INDEX}.csv', index=False)
+    convert_communicable()    
